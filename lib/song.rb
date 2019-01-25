41ea51fd928403e5f0b26d60572ea2516a806658
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-25 07:35:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,12 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    @@all.map {|x| x.name == name} ? 
+    item = @@all.find {|x| x.name == name} 
+    if item
+      return item
+    else
+      self.create_by_name(name)
+    end
+  end
   
 end
